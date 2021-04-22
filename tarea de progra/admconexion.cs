be705aa7e16d85e241d277cb2e2ba4d46d647a65
kerlin17 <80788638+kerlin17@users.noreply.github.com>
2021-04-22 18:28:33 -0500

    Add files via upload

---
+++ tarea de progra/admconexion.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Forms;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    class Class1
+    {
+        public MySqlConnection oconexion;
+        public bool crearconexion()
+        {
+        bool Conectado = false;
+        String servidor = "localhost", puerto = "3306", usuario = "root";
+        String clave = "root", basedatos = "TABLA PRODUCTO";
+        String candenaconexion = String.Format("datasource={0};port={1};username={2};password={3};database={4}", servidor, puerto, usuario, clave, basedatos);
+        try 
+        {
+        oconexion= new MySqlConnection(cadenaconexion);
+        oconexion.Open();
+            Conectado= true;
+        }
+    catch (MySqlConnection Exeption)
+    {
+    MessageBox.Show
+        throw;
+    }
+            return Conectado;
+
+    }
+    }
